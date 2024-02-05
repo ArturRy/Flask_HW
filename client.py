abf476d1defa5a3e60053a2bad8704f8f3655b94
Artur <mini--me@mail.ru>
2024-02-05 21:30:22 +0300

    commit

---
+++ client.py
@@ -0,0 +1,19 @@
+import requests
+
+# response = requests.post('http://127.0.0.1:5000/advert/',
+#                          json={'title': 'nice weather',
+#                                'description': 'weather forecast',
+#                                'owner': 'Gismeteo'}
+#                          )
+
+# response = requests.get('http://127.0.0.1:5000/advert/1/')
+
+# response = requests.patch('http://127.0.0.1:5000/advert/1/',
+#                          json={'title': 'bad weather',
+#                                'description': 'weather forecast',
+#                                'owner': 'Gismeteo'
+#                                })
+# response = requests.delete('http://127.0.0.1:5000/advert/1/')
+# response = requests.get('http://127.0.0.1:5000/advert/1/')
+# print(response.status_code)
+# print(response.text)
